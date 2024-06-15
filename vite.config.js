51ebd35ef12700b9ca633a66e4342e48249c7400
Devishree19 <devisrikathir19@gmail.com>
2024-06-15 19:36:46 +0530

    Production3

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    
-  },
+  
   server: {
     host: '0.0.0.0'
   },
